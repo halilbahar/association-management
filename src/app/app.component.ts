92a026a5a15a43d871c23f3574f9dce78e075430
Halil Bahar <halil.s.bahar@gmail.com>
2024-11-08 23:19:37 +0100

    Create basic app structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSidenav, MatSidenavContainer, MatSidenavContent } from '@angular/material/sidenav';
+import { MatListItem, MatNavList } from '@angular/material/list';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSidenavContainer, MatSidenavContent, MatSidenav, MatNavList, MatListItem, MatIcon],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  host: {
+    class: 'page'
+  }
 })
 export class AppComponent {
-  title = 'association-management';
 }
