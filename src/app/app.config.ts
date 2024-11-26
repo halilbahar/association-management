48c202bf29b6fd7a6b20b5bbf600473c8f6b8fa4
Halil Bahar <halil.s.bahar@gmail.com>
2024-11-26 17:14:47 +0100

    Refactor locale registration for de-AT
    Moved the locale import and registration for `de-AT` from a global import to a programmatic registration using `registerLocaleData`. This change ensures better modularity and clearer code structure by explicitly defining the locale within the configuration.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,5 @@
 import { APP_INITIALIZER, ApplicationConfig, LOCALE_ID, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import '@angular/common/locales/global/de-AT';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
@@ -10,6 +9,11 @@ import { DatabaseService } from '~services/database.service';
 import { provideTranslateService, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { HttpClient, provideHttpClient } from '@angular/common/http';
+
+import localeDe from '@angular/common/locales/de';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeDe, 'de-AT');
 
 export const appConfig: ApplicationConfig = {
   providers: [
